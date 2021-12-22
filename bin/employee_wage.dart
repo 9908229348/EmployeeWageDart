caf296aa49429a41ba2784d621af5a6269cabae5
9908229348 <kalavaisravanthi98@gmail.com>
2021-12-22 18:32:36 +0530

    Refactored: Added part Time employee Hrs

--- bin/employee_wage.dart
+++ bin/employee_wage.dart
@@ -1,17 +1,21 @@
 import 'dart:math';
 
 void main(List<String> arguments) {
-  final int isPresent = 1;
+  final int isFullTime = 1;
+  final int isPartTime = 2;
   final int empWagePerHr = 20;
   final int empFullDayHrs = 8;
+  final int empPartTimeHrs = 4;
+  int empWorkingHrs;
   int empWage;
-  int employeeCheck = Random().nextInt(2);
-  if(employeeCheck == isPresent){
-    print("Employee is Present");
-    empWage = empWagePerHr * empFullDayHrs;
+  int employeeCheck = Random().nextInt(3);
+  if(employeeCheck == isFullTime){
+    empWorkingHrs = empFullDayHrs;
+  } else if(employeeCheck == isPartTime){
+    empWorkingHrs = empPartTimeHrs;
   } else {
-    print("Employee is Absent");
-    empWage = 0;
+    empWorkingHrs = 0;
   }
+  empWage = empWorkingHrs * empWagePerHr;
   print("Employee Wage is : $empWage");
 }
