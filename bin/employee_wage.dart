a3554e26667964583cd469dd49cf86c5cf2de1f3
9908229348 <kalavaisravanthi98@gmail.com>
2021-12-22 18:43:15 +0530

    Refactored: Employee Wage using Switch Case

--- bin/employee_wage.dart
+++ bin/employee_wage.dart
@@ -1,20 +1,17 @@
 import 'dart:math';
-
 void main(List<String> arguments) {
-  final int isFullTime = 1;
-  final int isPartTime = 2;
   final int empWagePerHr = 20;
   final int empFullDayHrs = 8;
   final int empPartTimeHrs = 4;
   int empWorkingHrs;
   int empWage;
   int employeeCheck = Random().nextInt(3);
-  if(employeeCheck == isFullTime){
-    empWorkingHrs = empFullDayHrs;
-  } else if(employeeCheck == isPartTime){
-    empWorkingHrs = empPartTimeHrs;
-  } else {
-    empWorkingHrs = 0;
+  switch(employeeCheck){
+    case 1: empWorkingHrs = empFullDayHrs;
+            break;
+    case 2: empWorkingHrs = empPartTimeHrs;
+            break;
+    default: empWorkingHrs = 0;
   }
   empWage = empWorkingHrs * empWagePerHr;
   print("Employee Wage is : $empWage");
