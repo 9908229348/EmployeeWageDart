c00e21f11c7c644d929f3e3ca39c8d6228c43154
9908229348 <kalavaisravanthi98@gmail.com>
2021-12-22 19:16:55 +0530

    Refactored: Calculate wages till Max Hrs or MaxDays Reached

--- bin/employee_wage.dart
+++ bin/employee_wage.dart
@@ -1,28 +1,28 @@
 import 'dart:math';
 void main(List<String> arguments) {
-  final int empWagePerHr = 20, numOfWorkingDays = 20;
+  final int empWagePerHr = 20, numOfWorkingDays = 20, maxHrsInMonth = 100;
   final int empFullDayHrs = 8;
   final int empPartTimeHrs = 4;
-  int empWorkingHrs = 0;
+  int empWorkingHrs = 0, empWorkingDays = 0;
   int empWage;
   int employeeCheck;
   int getEmployeeHrs(int checkHrs){
     switch(checkHrs){
       case 1: empWorkingHrs = empFullDayHrs;
-              print("Full Time Working");
               break;
       case 2: empWorkingHrs = empPartTimeHrs;
-              print("Part Time Working");
               break;
       default: empWorkingHrs = 0;
-               print("Employee Absent");
     }
     return empWorkingHrs;
   }
-  for(int i = 0; i < numOfWorkingDays; i++){
+  while(empWorkingHrs <= maxHrsInMonth && empWorkingDays < numOfWorkingDays){
+    empWorkingDays++;
     employeeCheck = Random().nextInt(3);
     empWorkingHrs += getEmployeeHrs(employeeCheck);
   }
+  print("Total working Hours $empWorkingHrs");
+  print("Employee Working Days are : $empWorkingDays");
   empWage = empWorkingHrs * empWagePerHr;
   print("Employee Wage is : $empWage");
 }
