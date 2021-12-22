c2f4f9523a694b75557941a10354c0610f1d8c45
9908229348 <kalavaisravanthi98@gmail.com>
2021-12-22 18:07:40 +0530

    Added: Program to check Employee is Present or Absent

--- bin/employee_wage.dart
+++ bin/employee_wage.dart
@@ -1,5 +1,12 @@
-
+import 'dart:math';
 
 void main(List<String> arguments) {
-  print("Welcome to EmployeeWage Problems");
+  print("Welcome to employee Wage Problems");
+  final int isPresent = 1;
+  int employeeCheck = Random().nextInt(2);
+  if(employeeCheck == isPresent){
+    print("Employee is Present");
+  } else {
+    print("Employee is Absent");
+  }
 }
