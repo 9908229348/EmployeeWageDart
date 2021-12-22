8784a39120a1e854efc71316ba8250bb6586c6ca
9908229348 <kalavaisravanthi98@gmail.com>
2021-12-22 19:06:50 +0530

    Refactored: Calculate Wages for Month

--- bin/employee_wage.dart
+++ bin/employee_wage.dart
@@ -1,17 +1,27 @@
 import 'dart:math';
 void main(List<String> arguments) {
-  final int empWagePerHr = 20;
+  final int empWagePerHr = 20, numOfWorkingDays = 20;
   final int empFullDayHrs = 8;
   final int empPartTimeHrs = 4;
-  int empWorkingHrs;
+  int empWorkingHrs = 0;
   int empWage;
-  int employeeCheck = Random().nextInt(3);
-  switch(employeeCheck){
-    case 1: empWorkingHrs = empFullDayHrs;
-            break;
-    case 2: empWorkingHrs = empPartTimeHrs;
-            break;
-    default: empWorkingHrs = 0;
+  int employeeCheck;
+  int getEmployeeHrs(int checkHrs){
+    switch(checkHrs){
+      case 1: empWorkingHrs = empFullDayHrs;
+              print("Full Time Working");
+              break;
+      case 2: empWorkingHrs = empPartTimeHrs;
+              print("Part Time Working");
+              break;
+      default: empWorkingHrs = 0;
+               print("Employee Absent");
+    }
+    return empWorkingHrs;
+  }
+  for(int i = 0; i < numOfWorkingDays; i++){
+    employeeCheck = Random().nextInt(3);
+    empWorkingHrs += getEmployeeHrs(employeeCheck);
   }
   empWage = empWorkingHrs * empWagePerHr;
   print("Employee Wage is : $empWage");
